5815a8d0e5ffef3433ddee64db6db0b83ab75c48
Malik <malikhussein229@gmail.com>
2024-08-01 21:34:49 +0300

    Task#4 Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+4-define_variables.py
+
+A module that defines and initializes several variables.
+
+This module is used to declare variables of different data types.
+
+Author: Malik Hussein
+"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
