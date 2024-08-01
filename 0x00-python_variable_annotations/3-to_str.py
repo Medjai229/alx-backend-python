3ba1e0cd73f57ab76ac390f5a376597029072c76
Malik <malikhussein229@gmail.com>
2024-08-01 21:27:25 +0300

    Task#3 Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+3-to_str.py
+
+A simple string conversion module that provides a to_str function.
+
+This module is used to convert numbers to their string representations.
+
+Author: Malik Hussein
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float number to a string.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        str: The string representation of the number.
+
+    Example:
+        >>> to_str(3.14)
+        '3.14'
+    """
+    return str(n)
