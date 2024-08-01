ae6cdf867598cc2bc065df0e20a2537e53488d88
Malik <malikhussein229@gmail.com>
2024-08-01 21:08:25 +0300

    Task#0 Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+0-add.py
+
+A simple math module that provides an add function.
+
+This module is used to perform basic arithmetic operations.
+
+Author: Malik Hussein
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers together.
+
+    Args:
+        a (float): The first number to add.
+        b (float): The second number to add.
+
+    Returns:
+        float: The sum of a and b.
+
+    Example:
+        >>> add(2.5, 3.7)
+        6.2
+    """
+    return a + b
